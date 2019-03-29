1576e9b203478ceb931bd131385f2f7fc8f9ea89
Stephen Telesmanic <steve.telesmanic@gmail.com>
2019-03-29 21:03:51 +0000

    created scraper for index page of whiskies

--- whisky_picker/lib/whisky_picker/whisky_scraper.rb
+++ whisky_picker/lib/whisky_picker/whisky_scraper.rb
@@ -1,17 +1,25 @@
 # scraper class
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 class WhiskyPicker::WhiskyScraper
-  attr_accessor :name, :country, :region_type, :proof, :rating, :description
 
-  def self.scrape_index (index_url = https://www.thewhiskyexchange.com/c/40/single-malt-scotch-whisky?filter=true&rfdata=~size.76#productlist-filter)
+  attr_accessor :name, :profile_url, :country, :region_type, :proof, :rating, :description
+
+  def self.scrape_index (index_url = "https://www.thewhiskyexchange.com/c/40/single-malt-scotch-whisky?filter=true&rfdata=~size.76#productlist-filter")
     whiskies = []
     index = Nokogiri::HTML(open(index_url))
-
     get_whiskies = index.css(".item")
     get_whiskies.each do |whisky|
-      my_whisky = whisky.new
-      binding.pry
-      my_whisky.name = whisky.css("a").attr("href").text.strip
+      my_whisky = self.new
+      my_whisky.name = whisky.css("a .name").text.strip
+      my_whisky.profile_url = whisky.css("a").attr("href").text.strip
+      whiskies << my_whisky
+    end
+    whiskies
+  end
+
+
 
 end
