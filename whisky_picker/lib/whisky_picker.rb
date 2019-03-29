4cffd148932524fe089453151aa86a118a84c5a9
Stephen Telesmanic <steve.telesmanic@gmail.com>
2019-03-29 15:50:43 +0000

    bundled gem and deleted original .gitignore

---
+++ whisky_picker/lib/whisky_picker.rb
@@ -0,0 +1,6 @@
+require "whisky_picker/version"
+
+module WhiskyPicker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ whisky_picker/lib/whisky_picker/version.rb
@@ -0,0 +1,3 @@
+module WhiskyPicker
+  VERSION = "0.1.0"
+end
