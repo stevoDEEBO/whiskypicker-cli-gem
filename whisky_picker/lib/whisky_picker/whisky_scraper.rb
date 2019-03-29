d0f8eb372cd060788b9368a342c2cf54124ef741
Stephen Telesmanic <steve.telesmanic@gmail.com>
2019-03-29 20:05:02 +0000

    starting to create scraper class

--- whisky_picker/lib/whisky_picker/whisky_scraper.rb
+++ whisky_picker/lib/whisky_picker/whisky_scraper.rb
@@ -1,5 +1,17 @@
 # scraper class
+require 'pry'
 
 class WhiskyPicker::WhiskyScraper
-  
+  attr_accessor :name, :country, :region_type, :proof, :rating, :description
+
+  def self.scrape_index (index_url)
+    whiskies = []
+    index = Nokogiri::HTML(open(index_url))
+
+    get_whiskies = index.css(".item")
+    get_whiskies.each do |whisky|
+      my_whisky = whisky.new
+      binding.pry
+      my_whisky.name = whisky.css("a").attr("href")
+
 end
