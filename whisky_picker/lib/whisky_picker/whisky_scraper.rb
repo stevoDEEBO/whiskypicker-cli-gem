f73e32781d14da799a84dfda74f5c604394ad4a1
Stephen Telesmanic <steve.telesmanic@gmail.com>
2019-04-01 18:22:03 +0000

    started profile scraper

--- whisky_picker/lib/whisky_picker/whisky_scraper.rb
+++ whisky_picker/lib/whisky_picker/whisky_scraper.rb
@@ -7,9 +7,10 @@ class WhiskyPicker::WhiskyScraper
 
   attr_accessor :name, :profile_url, :country, :region_type, :proof, :rating, :description
 
-  def self.scrape_index (index_url = "https://www.thewhiskyexchange.com/c/40/single-malt-scotch-whisky?filter=true&rfdata=~size.76#productlist-filter")
+  def self.scrape_index (index_url = "https://www.thewhiskyexchange.com/c/40/single-malt-scotch-whisky?filter=true&rfdata=~size.76~pr.100#productlist-filter")
     whiskies = []
     index = Nokogiri::HTML(open(index_url))
+
     get_whiskies = index.css(".item")
     get_whiskies.each do |whisky|
       my_whisky = self.new
@@ -20,6 +21,18 @@ class WhiskyPicker::WhiskyScraper
     whiskies
   end
 
+  def self.scrape_profile (index_url = "https://www.thewhiskyexchange.com/c/40/single-malt-scotch-whisky?filter=true&rfdata=~size.76#productlist-filter")
+    profile = Nokogiri::HTML(open(index_url))
 
+    whisky = index.css(".container")
+    this_whisky = self.new
+    this_whisky.name = whisky.css(".name-container h1").text.strip
+    this_whisky.country = whisky.css(".details dl.meta").nth-child(6).text.strip
+    this_whisky.region_type = whisky.css("").text.strip
+    this_whisky.proof = whisky.css("").text.strip
+    this_whisky.rating = whisky.css("").text.strip
+    this_whisky.description = whisky.css("").text.strip
 
+    this_whisky
+  end
 end
