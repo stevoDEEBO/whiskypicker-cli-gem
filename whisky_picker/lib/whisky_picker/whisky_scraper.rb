4efdf3765e765df288e73300a8a0633a16f10d41
Stephen Telesmanic <steve.telesmanic@gmail.com>
2019-04-01 23:28:41 +0000

    finished first functioning run of scraper class

--- whisky_picker/lib/whisky_picker/whisky_scraper.rb
+++ whisky_picker/lib/whisky_picker/whisky_scraper.rb
@@ -21,17 +21,17 @@ class WhiskyPicker::WhiskyScraper
     whiskies
   end
 
-  def self.scrape_profile (index_url = "https://www.thewhiskyexchange.com/c/40/single-malt-scotch-whisky?filter=true&rfdata=~size.76#productlist-filter")
-    profile = Nokogiri::HTML(open(index_url))
+  def self.scrape_profile (profile_url = "https://www.thewhiskyexchange.com/p/4132/oban-14-year-old")
+    profile = Nokogiri::HTML(open(profile_url))
 
-    whisky = index.css(".container")
+    whisky = profile.css(".container")
     this_whisky = self.new
     this_whisky.name = whisky.css(".name-container h1").text.strip
-    this_whisky.country = whisky.css(".details dl.meta").nth-child(6).text.strip
-    this_whisky.region_type = whisky.css("").text.strip
-    this_whisky.proof = whisky.css("").text.strip
-    this_whisky.rating = whisky.css("").text.strip
-    this_whisky.description = whisky.css("").text.strip
+    this_whisky.country = whisky.css("dl.meta").at('dt:contains("Country")').next_element.text.strip
+    this_whisky.region_type = whisky.css(".name-container .properties li").first.text.strip
+    this_whisky.proof = whisky.css(".name-container .strength").text.split(" / ").last.strip
+    this_whisky.rating = whisky.css(".rating-container span").text.strip
+    this_whisky.description = whisky.css("#prodDesc").text.strip
 
     this_whisky
   end
