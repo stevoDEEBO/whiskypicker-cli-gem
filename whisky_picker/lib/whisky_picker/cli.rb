3ca17a0c93c39d197fe5cbb109bd9094dfe71b45
Stephen Telesmanic <steve.telesmanic@gmail.com>
2019-04-02 16:52:56 +0000

    added call method process flow and greeting

--- whisky_picker/lib/whisky_picker/cli.rb
+++ whisky_picker/lib/whisky_picker/cli.rb
@@ -2,7 +2,19 @@
 
 class WhiskyPicker::CLI
 
+BASEPATH = 'http://www.thewhiskyexchange.com'
+
   def call
+
+    greeting
+    pick
+    menu
+    laters
+  end
+
+  def greeting
     puts "Welcome to Whisky Picker...ready to find some whisky?"
   end
+
+  
 end
